dafa888a69b932334e2a5aba8e2874d2da86e0fb
JelleMedendorp <65771904+JelleMedendorp@users.noreply.github.com>
2022-10-18 13:30:52 +0200

    Put screens in seperate files

--- App.js
+++ App.js
@@ -1,23 +1,9 @@
 import * as React from 'react';
-import { View, Text } from 'react-native';
+import { Button, View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
-function HomeScreen() {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
-    </View>
-  );
-}
-
-function DetailsScreen() {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Details Screen</Text>
-    </View>
-  );
-}
+import HomeScreen from './screens/home'
+import DetailsScreen from './screens/other'
 
 const Stack = createNativeStackNavigator();
 
---
+++ screens/other.js
@@ -0,0 +1,21 @@
+import * as React from 'react';
+import { Button, View, Text } from 'react-native';
+
+function DetailsScreen({ route, navigation}) {
+    const { itemId} = route.params;
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Details Screen</Text>
+        <Text>itemId: {JSON.stringify(itemId)}</Text>
+        <Button
+          title="Go to Details... again"
+          onPress={() =>
+            navigation.push('Details', {
+              itemId: Math.floor(Math.random() * 100),
+            })
+          }
+        />
+      </View>
+    );
+  }
+export default DetailsScreen;
