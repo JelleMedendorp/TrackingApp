add59b7cbcc08b02e40e1a86198d93a0149bf03e
Jelle Medendorp <65771904+JelleMedendorp@users.noreply.github.com>
2022-11-15 23:30:45 +0100

    Working on interval algorithm...

--- screens/other.js
+++ screens/other.js
@@ -7,8 +7,20 @@ import * as shape from 'd3-shape'
 
 function DetailsScreen() {
   const datasetter = [0]
-  const [array, setArray] = useState(datasetter)
+  const [xdata, setXdata] = useState(datasetter)
+  const [ydata, setYdata] = useState(datasetter)
+  const [zdata, setZdata] = useState(datasetter)
+  const [hits, setHits] = useState(0)
   const [subscription, setSubscription] = useState(null);
+
+  let time = 0;
+  let interval = 0;
+  const waveLength = 500;
+  const hitTime = 2000;
+  const buffer = 500;
+  const listenTime = 9000;
+  let beginTime = Date.now();
+  global.timesHit = 0;
 
   let data = {x: 0, y: 0, z: 0};
 
@@ -20,16 +32,38 @@ function DetailsScreen() {
     Gyroscope.setUpdateInterval(100);
   };
 
+  const _interval = (y) => {
+    interval = Date.now() - time + waveLength;
+    time = Date.now();
+    if((1500 < interval) && (interval < 2500)){
+      console.log("hit!");
+      let tempHit = hits
+      console.log(tempHit);
+      setHits(1);
+      console.log(hits);
+
+    }
+              
+  }
+
   const _subscribe = () => {
     setSubscription(
         Gyroscope.addListener(gyroscopeData => {
         data = gyroscopeData;
         const {x, y, z} = data
-        setArray(oldArray => [...oldArray, y]);
-        if(array.length > 10){
-          setArray(oldArray => [...oldArray.slice(1)]);
+      //  console.log(Date.now());
+        if(y > 1 || y < -1){
+          _interval(y) ;
         }
-        
+//        console.log(hits);
+        if(hits == 1){
+          beginTime = Date.now();
+        }
+//        console.log(Date.now() - beginTime);
+
+        setXdata(oldArray => [...oldArray, x]);
+        setYdata(oldArray => [...oldArray, y]);
+        setZdata(oldArray => [...oldArray, z]);
       })
     );
   };
@@ -41,21 +75,36 @@ function DetailsScreen() {
 
   useEffect(() => { 
     _subscribe();
-    _handleUpdate();
+//  _handleUpdate();
     return () => _unsubscribe();
   }, []);
   
-  const _handleUpdate = () => {
-    if(array.length > 200) {
-      setArray(oldArray => [...oldArray.slice(1)]);
-    } 
-  };
+ // const _handleUpdate = () => {
+ //   if(ydata.length > 200) {
+ //     setZdata(oldArray => [...oldArray.slice(1)]);
+ //   } 
+ // };
   const contentInset = { top: 20, bottom: 20 }
+
+  const graphData = [
+    {
+        data: xdata.slice(-200),
+        svg: { stroke: 'blue'},
+    },
+    {
+      data: ydata.slice(-200),
+      svg: { stroke: 'green'},
+    },
+    {
+      data: zdata.slice(-200),
+      svg: { stroke: 'red'},
+    },
+  ]
 
 return (    
   <View style={{ height: 200, flexDirection: 'row' }}>
     <YAxis
-      data={array}
+      data={graphData}
       contentInset={contentInset}
       svg={{
           fill: 'grey',
@@ -66,7 +115,7 @@ return (
     />               
     <LineChart
       style={{ flex: 1, marginLeft: 16 }}
-      data={array.slice(-200)}
+      data = {graphData}
       svg={{ stroke: 'rgb(134, 65, 244)' }}
       contentInset={contentInset}
       numberOfTicks={10}
