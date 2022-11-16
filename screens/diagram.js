34c6c05b91eaaddc1b242a2c70320710b328f7f7
JelleMedendorp <65771904+JelleMedendorp@users.noreply.github.com>
2022-11-16 14:57:57 +0100

    Added standalone diagram

--- screens/diagram.js
+++ screens/diagram.js
@@ -1,40 +1,103 @@
 import React, { useState, useEffect } from 'react';
-import { Button, View, Text, Dimensions } from 'react-native';
-import { LineChart, Grid } from 'react-native-svg-charts'
+import { Button, View, Text, TouchableOpacity,Alert } from 'react-native';
+import { LineChart, Grid,AreaChart, YAxis } from 'react-native-svg-charts'
 import { Gyroscope } from 'expo-sensors';
 
 
 
 
-function DiagramScreen({ route, navigation}) {
-  
-  const dataset = [50, 10, 40, 95, -4, -24, 85, 91, 35, 53, -53, 24, 50, -20, -80, 90]
-  const [data, setData] = useState(dataset)
+function DiagramScreen() {
+  const datasetter = [0]
+  const [xdata, setXdata] = useState(datasetter)
+  const [ydata, setYdata] = useState(datasetter)
+  const [zdata, setZdata] = useState(datasetter)
+  const [subscription, setSubscription] = useState(null);
 
+  let data = {x: 0, y: 0, z: 0};
 
-  const _handleUpdate = () => {
-    console.log('added data');
-    setData(oldArray => [...oldArray, 20])
+  const _slow = () => {
+    Gyroscope.setUpdateInterval(1000);
   };
 
-  return (
-    <View>
+  const _fast = () => {
+    Gyroscope.setUpdateInterval(100);
+  };
+
+  const _subscribe = () => {
+    setSubscription(
+        Gyroscope.addListener(gyroscopeData => {
+        data = gyroscopeData;
+        const {x, y, z} = data
+        setXdata(oldArray => [...oldArray, x]);
+        setYdata(oldArray => [...oldArray, y]);
+        setZdata(oldArray => [...oldArray, z]);
+
+
+      })
+    );
+  };
+
+  const _unsubscribe = () => {
+    subscription && subscription.remove();
+    setSubscription(null);
+  };
+
+  useEffect(() => { 
+    _subscribe();
+    return () => _unsubscribe();
+  }, []);
+
+  const contentInset = { top: 20, bottom: 20 }
+  const graphData = [
+    {
+        data: xdata.slice(-200),
+        svg: { stroke: 'blue'},
+    },
+    {
+      data: ydata.slice(-200),
+      svg: { stroke: 'green'},
+    },
+    {
+      data: zdata.slice(-200),
+      svg: { stroke: 'red'},
+    },
+  ]
+return (   
+  <View> 
+  <View style={{ height: 200, flexDirection: 'row' }}>
+    <YAxis
+      data={ydata.slice(-200)}
+      contentInset={contentInset}
+      svg={{
+          fill: 'grey',
+          fontSize: 10,
+      }}
+      numberOfTicks={10}
+      formatLabel={(value) => `${value}`}
+    />               
     <LineChart
-    style={{ height: 200 }}
-    data={data}
-    svg={{ stroke: 'rgb(134, 65, 244)' }}
-    contentInset={{ top: 20, bottom: 20 }}
->
+      style={{ flex: 1, marginLeft: 16 }}
+      data = {ydata.slice(-200)}
+      svg={{ stroke: 'rgb(134, 65, 244)' }}
+      contentInset={contentInset}
+      numberOfTicks={10}
+    >
     <Grid />
-</LineChart>
-<Button
-      onPress={_handleUpdate}
-      title="Learn More"
-      color="#841584"
-      accessibilityLabel="Learn more about this purple button"
-/></View>
+    </LineChart>
+    </View>
+    <View>
 
-  );
-  }
+    <TouchableOpacity onPress={_slow}>
+      <Text>Slow</Text>
+    </TouchableOpacity>
+    <TouchableOpacity onPress={_fast}>
+      <Text>Fast</Text>
+    </TouchableOpacity>
+    <TouchableOpacity onPress={subscription ? _unsubscribe : _subscribe} >
+      <Text>{subscription ? 'On' : 'Off'}</Text>
+    </TouchableOpacity>
+  </View>
+  </View>
+);
+}
 export default DiagramScreen;
-
